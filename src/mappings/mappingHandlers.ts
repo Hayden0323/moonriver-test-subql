43ebe7ad15250c615df32b87e5c4dc099633ee01
Hayden Fan <haosong0323@gmail.com>
2022-01-06 10:57:35 +0800

    feat: moonriver

---
+++ src/mappings/mappingHandlers.ts
@@ -0,0 +1,13 @@
+import { MoonbeamEvent } from '@subql/contract-processors/dist/moonbeam'
+import { createTransfer, TransferEventArgs } from '../utils/createTransferRecord'
+
+export async function handleTransferEvent(event: MoonbeamEvent<TransferEventArgs>): Promise<void> {
+  if (!event.args) return
+
+  const from = event.args.from
+  const to = event.args.to
+  const amount = event.args.value.toBigInt()
+  const tokenAddress = event.address
+
+  await createTransfer(from, to, tokenAddress, amount, event)
+}
